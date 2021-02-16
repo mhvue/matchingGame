e4ca843c683c107f9bcb5337d3f647dc65f1f9e1
Mai Houa Vue <mhvue4@gmail.com>
2021-02-15 19:36:39 -0600

    added numbers to test matching on the cards.

--- app.js
+++ app.js
@@ -2,18 +2,11 @@
 
 const card = document.querySelector(".card");
 const allCards = document.querySelectorAll(".card-content");
-console.log(allCards)
 
-//just one instance of that class
-// card.addEventListener("click", function(){
-//     console.log("hello")
-// });
-
-//now you add click event to each instance of .card
+//now you add click event to each instance of .card-content
 allCards.forEach(card =>{
     card.addEventListener("click",function(){
-        console.log(card.classList);
-        //get to children to add active class to backend
-       card.classList.toggle("active")
+        //toggle class of active on card-content
+       card.classList.toggle("active");
     })
 });
