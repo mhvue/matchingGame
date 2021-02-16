585cb40162f82d715b1f868f62371377a768ca26
Mai Houa Vue <mhvue4@gmail.com>
2021-02-16 17:18:46 -0600

    numbers on now showing up on back card.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const card = document.querySelector(".card");
 const allCards = document.querySelectorAll(".card-content");
-const numbers = document.querySelectorAll(".num");
 const numArr = [1,1,2,2,3,3,4,4,5,5]
 //now you add click event to each instance of .card-content
 allCards.forEach(card =>{
@@ -17,10 +16,21 @@ allCards.forEach(card =>{
 let sorted = numArr.sort( () => 0.5 - Math.random());
 console.log(sorted)
 
-sorted.forEach(num =>{
-    console.log(num)
-    //input on the back card
-})
+sorted.forEach((num, index) =>{
+    //console.log(num);
+    //input on each back card
+    const info = `<h2 class='num'>${num}</h2>`;
+   console.log(info)
+   const selector = `.card:nth-child(${index+1}) .back-card`;
+   console.log(selector)
+
+   const element = document.querySelector(selector);
+
+   console.log(element)
+   element.innerHTML = info;
+});
+
+
 
 //figure out a way to say if num match num then win, if not, then no win 
 //gray out cards that matched 
