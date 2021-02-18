6cc341a3ef1dbf1ac4946aaa81e80884f7d744ff
Mai Houa Vue <mhvue4@gmail.com>
2021-02-17 20:21:01 -0600

    score increaseing as cards are matched. still working on graying out cards.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const card = document.querySelector(".card");
 const allCards = document.querySelectorAll(".card-content");
 const backCards = document.querySelectorAll(".back-card");
+const scoreCard = document.getElementById("scoreHere")
 const numArr = [1,1,2,2,3,3,4,4,5,5];
 let collectNum =[];
+let score = 0;
 
 //move or shuffle the array of numbers and add it to back-card 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -14,7 +16,7 @@ console.log(sorted)
 sorted.forEach((num, index) =>{
     //console.log(num);
     //input on each back card
-    const info = `<h2 class='num' data-num=${num}>${num}</h2>`;
+   const info = `<h2 class='num' data-num=${num}>${num}</h2>`;
    const selector = `.card:nth-child(${index+1}) .back-card`;
    const element = document.querySelector(selector);
    element.innerHTML = info;
@@ -24,30 +26,39 @@ sorted.forEach((num, index) =>{
 //now you add click event to each instance of .card-content
 //to get card to flip
 allCards.forEach(card =>{
-    card.addEventListener("click",function(){
+    card.addEventListener("click",function(e){
         //toggle class of active on card-content
        card.classList.toggle("active");
 
        const dataNum = card.children[1].children[0].dataset.num;
        collectNum.push(parseInt(dataNum));
-       console.log(collectNum)
-       //reset length of arry to have 2 element to compare 
-    
-        // if(collectNum[0] !== collectNum[1] && collectNum.length > 2){
-        //     collectNum = [];
-        // }
-        if(collectNum[0] === collectNum[1]){
-            console.log("win!")
+     
+        //gotta make sure can't click on same card twice
+
+         if(collectNum.length === 2 && collectNum[0] == collectNum[1]){
             console.log(collectNum[0],collectNum[1])
+            console.log("win!")
+            score++;
+            scoreCard.textContent = score;
+    
+            collectNum = []; //reset length of collectNum back to 0
+            //get cards that matched to gray out 
+            //add grayout class to those cards by attr 
+            card.children[1].classList.add("grayOut");
+            //reset score back to 0 if game is all done
+            console.log(collectNum)
         }
-        console.log(collectNum)
+        else if (collectNum.length === 2 && collectNum[0] != collectNum[1]){
+            console.log("no match")
+            collectNum = []; //reset length of collectNum back to 0
+            //since cards did not match, flip back to green side 
+            e.target.classList.remove("active")
+            console.log(collectNum)
+        }
     
     });
 });
 
- //figure out a way to say if num match num then win, if not, then no win 
-
-
-
+ 
 
 //gray out cards that matched 
